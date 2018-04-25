d555dbaa668dc60b9c9df1d47812a35d4e3fa591
kaisanjay <sanjaykai.1994@gmail.com>
2018-04-26 00:54:07 +0530

    new changes es6

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -5,11 +5,11 @@ class TodoItems extends Component {
 	constructor(props) {
 		super(props);
 
-		this.createTasks = this.createTasks.bind(this);
+		
 	}
 
 
-	createTasks(item) {
+	createTasks = (item) => {
 		return <li onClick={() => this.delete(item.key)}
 		                key={item.key}>{item.text}</li>
 	}
