36c7d24c8801eb7f8cc8fde85717292688619ef4
kaisanjay <sanjaykai.1994@gmail.com>
2018-04-26 00:56:01 +0530

    final

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -2,13 +2,6 @@ import React, {Component} from "react";
 import FlipMove from "react-flip-move";
 
 class TodoItems extends Component {
-	constructor(props) {
-		super(props);
-
-		
-	}
-
-
 	createTasks = (item) => {
 		return <li onClick={() => this.delete(item.key)}
 		                key={item.key}>{item.text}</li>
